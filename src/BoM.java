ce27d9afa708e9053c2712342428d67d0f2b7a72
Mahesh Muthukumar <bmmk1210@gmail.com>
2021-10-16 19:52:53 +0530

    Readability improvement

--- src/BoM.java
+++ src/BoM.java
@@ -6,6 +6,7 @@ public class BoM {
             Scanner sc = new Scanner(System.in);
             Class.forName("com.mysql.cj.jdbc.Driver");
             System.out.println("Enter db pwd:");
+
             String db_pwd=sc.next();
             Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mrp_db", "root",db_pwd);
             Statement stmt = con.createStatement();
@@ -14,6 +15,7 @@ public class BoM {
             System.out.println("Enter number of cycles to be assembled");
             int no_of_cycles = sc.nextInt();// Number of cycles to be assembled
             BicycleParts honda_cycle = new BicycleParts(no_of_cycles);
+
             int update_flag = 1; // flag to check if we want to update the stock in the inventory
             while(update_flag==1) // loop runs till flag is set zero
             {
@@ -21,6 +23,7 @@ public class BoM {
                 int stock_update=0;// variable to store the new value of the stock
                 System.out.println("Want to update the Inventory? " + '\n' + "Enter 1 for yes , 0 (zero) for No:");
                 update_flag = sc.nextInt(); //updates flag value
+
                 if (update_flag == 1) {
                     int i = 1;
                     for (String it : items) { //prints all the item in database
@@ -29,13 +32,16 @@ public class BoM {
                     }
                     System.out.println("Enter -1 to not update any item");// to stop from the loop if we choose not to update any item's stock
                     item_to_be_updated=sc.nextInt();
+
                     if(item_to_be_updated==-1){
                         break;
                     }
+
                     item_to_be_updated--;
                     System.out.println("Enter the stock to be updated in the item: ");
                     stock_update=sc.nextInt();
                     String quer="update mrp_db.cycle set stock=? where item='"+items[item_to_be_updated]+"'";
+
                     PreparedStatement ps = con.prepareStatement(quer);
                     ps.setInt(1,stock_update);
                     ps.executeUpdate();
@@ -45,6 +51,7 @@ public class BoM {
             for(String it:items) {//prints all items which needs to print inventory net off
                 String itm="select stock from mrp_db.cycle where item='"+it+"'";
                 ResultSet rs = stmt.executeQuery(itm);
+
                 switch (it) {
                     case "Frames": while (rs.next()) {System.out.println("Frames req: "+(honda_cycle.getFrame() - rs.getInt(1))+'\n');} break;
                     case "Seats": while (rs.next()) {System.out.println("Seats req: "+(honda_cycle.getSeat() - rs.getInt(1)));} break;
